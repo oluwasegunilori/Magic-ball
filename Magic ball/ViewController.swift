55c3125a4741660302b714ac70a25dc4ca6e9345
oluwasegun <ilorioluwasegun@gmail.com>
2022-09-23 18:03:45 +0100

    Initial Commit

---
+++ Magic ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic ball
+//
+//  Created by shegz on 23/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
